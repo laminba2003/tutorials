6921e33fe6f50dc5c3edfb76f1c4e872289845ab
Ibrahima <Ibrahima@192.168.1.13>
2016-08-09 18:20:06 +0000

    struts and maven integration

---
+++ struts2/helloWorld/src/actions/HelloWorldAction.java
@@ -0,0 +1,13 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class HelloWorldAction extends ActionSupport {
+	
+	public String execute() {
+		
+		return SUCCESS;
+	}
+
+}
